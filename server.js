3a1ad7df36bcd0ef9a00a1cde69a6e8e710b05a0
Filip Troníček <filip.tronicek@seznam.cz>
2020-04-22 22:10:30 +0200

    Root fs

--- server.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let app = express();
-const port = 5000 || process.env.port;
-
-app.use(express.static("./static"));
-
-app.listen(port, function() {
-    console.log(`Listening on port ${port}`);
-});
