ea292d8e3186dc5f37caffb0e257d66dfe103a6e
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-29 13:18:57 +0000

    Change to const where can be

--- predict.js
+++ predict.js
@@ -1,12 +1,12 @@
 $("#image-selector").change(function () {
   let reader = new FileReader();
   reader.onload = function () {
-    let dataURL = reader.result;
+    const dataURL = reader.result;
     $("#selected-image").attr("src", dataURL);
     $("#prediction-list").empty();
   };
 
-  let file = $("#image-selector").prop("files")[0];
+  const file = $("#image-selector").prop("files")[0];
   reader.readAsDataURL(file);
 });
 
