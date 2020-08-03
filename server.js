ecf188ba206f90d504fb44ad31e02368029fb9c8
Sascha Dittmann <Sascha.Dittmann@microsoft.com>
2020-08-03 12:11:10 +0000

    Added/Updated files to use VS Code Spaces

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 let express = require("express");
 let app = express();
+let port = 3000;
 
 app.use(express.static("./static"));
 
-app.listen(81, function() {
-    console.log("Listening on port 81");
+app.listen(port, function() {
+    console.log(`Listening at http://localhost:${port}`);
 });
